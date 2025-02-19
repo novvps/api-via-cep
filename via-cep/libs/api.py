adff493956a3cdb418f9e125494e9d3be125ce42
Paola <paolacrisnvs@gmail.com>
2025-02-19 19:11:00 -0300

    mudança de pastas

--- via-cep/libs/api.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-def buscar_cep(cep):
-    url = f'https://viacep.com.br/ws/{cep}/json/'
-    retorno = requests.get(url)
-    retorno = json.loads(retorno.text)
-    return retorno
